fee1ce36155a81c621a46d919e78ac4317599873
barak manos <>
2024-10-13 21:32:24 +0300

    Upgrade solc and hardhat versions

--- project/contracts/NaturalNum.sol
+++ project/contracts/NaturalNum.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity 0.8.27;
+pragma solidity 0.8.28;
 
 library NaturalNum {
     function encode(uint256 val) internal pure returns (uint256[] memory) { unchecked {
