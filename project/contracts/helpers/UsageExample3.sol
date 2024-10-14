8fec65bf46a379cce62de27c2fa199899c8aa026
barak manos <>
2024-10-14 06:07:27 +0300

    Improve test

--- project/contracts/helpers/UsageExample3.sol
+++ project/contracts/helpers/UsageExample3.sol
@@ -2,31 +2,19 @@
 pragma solidity 0.8.28;
 
 import "../NaturalNum.sol";
-import "../RationalNum.sol";
 
 contract UsageExample3 {
-    using RationalNum for Rnum;
     using NaturalNum for uint256[];
 
     struct Fraction {
-        uint256 n;
-        uint256 d;
+        uint256[] n;
+        uint256[] d;
     }
 
-    function exp(Rnum memory x, uint256 iterations) external pure returns (Fraction memory) {
-        Fraction memory f = exp(x.n, x.d, iterations);
-        return x.s ? Fraction(f.d, f.n) : Fraction(f.n, f.d);
-    }
-
-    function exp(uint256[] memory xn, uint256[] memory xd, uint256 iterations) private pure returns (Fraction memory) {
-        uint256[] memory n = xn;
-        uint256[] memory d = xn.add(xd.mul(NaturalNum.encode(iterations)));
-        for (uint256 i = iterations - 1; i > 0; --i) {
-            uint256[] memory next_n = xn.mul(d);
-            uint256[] memory next_d = xn.mul(d).add(xd.mul(d.sub(n)).mul(NaturalNum.encode(i)));
-            n = next_n;
-            d = next_d;
-        }
-        return Fraction(d.decode(), d.sub(n).decode());
+    function exp(Fraction memory x, uint256 iterations) external pure returns (Fraction memory) {
+        Fraction memory y = Fraction(NaturalNum.encode(0), NaturalNum.encode(1));
+        for (uint256 i = iterations; i > 0; --i)
+            y = Fraction(x.n.mul(y.d), x.n.mul(y.d).add(x.d.mul(y.d.sub(y.n)).mul(NaturalNum.encode(i))));
+        return Fraction(y.d, y.d.sub(y.n));
     }
 }
--- project/tests/UsageExample3.js
+++ project/tests/UsageExample3.js
@@ -2,9 +2,12 @@ const TestContract = artifacts.require("UsageExample3");
 const Utilities = require("./helpers/Utilities.js");
 const Decimal = require("decimal.js");
 
-const ITERATIONS = 32;
+const toBN = web3.utils.toBN;
 
-const toDecimal = actual => Decimal(actual[0].toString()).div(actual[1].toString());
+const ITERATIONS = 40;
+
+const toNatural = x => [...Array(Number(x.length)).keys()].reduce((a, i) => a.add(toBN(x[i]).shln(i * 256)), toBN(0));
+const toDecimal = x => Decimal(toNatural(x[0]).toString()).div(toNatural(x[1]).toString());
 
 describe(TestContract.contractName, () => {
     let testContract;
@@ -13,12 +16,12 @@ describe(TestContract.contractName, () => {
         testContract = await TestContract.new();
     });
 
-    for (let n = -4; n <= 4; n++) {
-        for (let d = 1; d <= 16; d++) {
-            it(`exp(${n} / ${d})`, async () => {
+    for (let n = 0; n <= 4; n++) {
+        for (let d = 1; d <= 20; d++) {
+            it(`exp(${n}/${d})`, async () => {
                 const expected = Decimal(n).div(d).exp();
-                const actual = toDecimal(await testContract.exp([n / d < 0, [Math.abs(n)], [Math.abs(d)]], ITERATIONS));
-                Utilities.assertAlmostEqual(actual, expected, "1e-18");
+                const actual = toDecimal(await testContract.exp([[n], [d]], ITERATIONS));
+                Utilities.assertAlmostEqual(actual, expected, "1e-26");
             });
         }
     }
